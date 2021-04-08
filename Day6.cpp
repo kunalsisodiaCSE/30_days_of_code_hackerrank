d321400b07fe343ca331b3d350f82fb24fae8bd0
Kunal Sisodia <71686673+kunalsisodiaCSE@users.noreply.github.com>
2021-04-08 10:53:17 +0530

    Add files via upload

---
+++ Day6.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        // getline(cin, s);
+        // cin<<s;
+        cin>>s;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (i % 2 == 0)
+            {
+                cout << s[i];
+            }
+        }
+        cout<<" ";
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (i % 2 != 0)
+            {
+                cout << s[i];
+            }
+        }
+        cout<<"\n";
+        // fflush(s);
+    }
+    return 0;
+}
