199c65f5624d0a082bc9d14c0c0ef691e956f9c9
Kunal Sisodia <71686673+kunalsisodiaCSE@users.noreply.github.com>
2021-04-11 09:32:08 +0530

    Add files via upload

---
+++ Day9.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the factorial function below.
+int factorial(int n) {
+
+if(n==0 ){
+
+    return 1;
+}
+else if(n==1){
+    return 1;
+}
+else return n*factorial(n-1);
+// return 78;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
